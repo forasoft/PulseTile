fbbe6e36ee13da4d71ef273e190b998cf371986b
Anastasiia <aholopolos@gmail.com>
2016-10-03 16:03:15 +0300

    added advanced-search-popup

---
+++ src/app/actions/search.js
@@ -0,0 +1,34 @@
+import {bindActionCreators} from 'redux';
+import * as types from '../constants/ActionTypes';
+
+export function advancedSearch(queryParams) {
+    return {
+        // Types of actions to emit before and after
+        types: [types.SEARCH, types.SEARCH_SUCCESS, types.SEARCH_ERROR],
+
+        // Check the cache (optional):
+        shouldCallAPI: (state) => !state.advancedSearch,
+
+        // Configure $http
+        config: {
+            method: 'post',
+            url: `api/patients/advancedSearch`,
+            data: queryParams
+        },
+
+        // Metadata to inject in begin/end actions
+        meta: {
+            timestamp: Date.now()
+        }
+    };
+}
+
+export default function searchActions($ngRedux) {
+    let actionCreator = {
+        advancedSearch
+    };
+
+    return bindActionCreators(actionCreator, $ngRedux.dispatch);
+}
+
+searchActions.$inject = ['$ngRedux'];
---
+++ src/app/redux/search.js
@@ -0,0 +1,36 @@
+import * as types from '../constants/ActionTypes';
+
+const INITIAL_STATE = {
+    isFetching: false,
+    error: false,
+    data: null
+};
+
+export default function posts(state = INITIAL_STATE, action) {
+    const {payload} = action;
+
+    var actions = {
+        [types.SEARCH]: (state) => {
+            return Object.assign({}, state, {
+                isFetching: true,
+                error: false
+            });
+        },
+        [types.SEARCH_SUCCESS]: (state) => {
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: payload.response
+            });
+        },
+        [types.SEARCH_ERROR]: (state) => {
+            return Object.assign({}, state, {
+                isFetching: false,
+                error: payload.error
+            });
+        },
+    };
+
+    return actions[action.type] ?
+        actions[action.type](state) :
+        state;
+}
