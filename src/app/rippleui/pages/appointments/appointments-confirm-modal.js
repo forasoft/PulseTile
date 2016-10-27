73d53ead17c9c2faadf888d35c546451aa1b68ab
vadimivanov <mr.vadim.ivanov@gmail.com>
2016-10-27 14:16:57 +0300

    fix pagination

--- src/app/rippleui/pages/appointments/appointments-confirm-modal.js
+++ src/app/rippleui/pages/appointments/appointments-confirm-modal.js
@@ -1,21 +1,3 @@
-'use strict';
-
-// angular.module('ripple-ui')
-//   .controller('AppointmentsConfirmModalCtrl', function ($scope, $modalInstance, time) {
-//
-//     $scope.modal = $modalInstance;
-//     $scope.time = new Date(time);
-//
-//     $scope.ok = function () {
-//       $modalInstance.close();
-//     };
-//
-//     $scope.cancel = function () {
-//       $modalInstance.dismiss('cancel');
-//     };
-//
-//   });
-
 export default function AppointmentConfirmModal($uibModal, serviceRequests) {
 var isModalClosed = true;
     var openModal = function (modal, time) {
