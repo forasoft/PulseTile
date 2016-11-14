2f152421a5c252be2372092e85fe4cf286038ca1
vadimivanov <mr.vadim.ivanov@gmail.com>
2016-11-14 18:50:30 +0200

    created karma.js test files

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,18 @@
 module.exports = config => {
   config.set({
+    basePath: '',
     frameworks: ['jasmine'],
 
     files: [
+      'bower_components/angular/angular.js',
+      'node_modules/angular-ui-router/release/angular-ui-router.js',
       'node_modules/babel-polyfill/dist/polyfill.js',
       'node_modules/sinon/pkg/sinon.js',
-      'karma.entry.js'
+      'bower_components/angular-mocks/angular-mocks.js',
+      'karma.entry.js',
+      // './src/app/rippleui/pages/**/*.js',
+      // './src/app/index.js',
+      './src/test/spec/**/*.js'
     ],
 
     preprocessors: {
@@ -18,21 +25,30 @@ module.exports = config => {
       noInfo: true
     },
 
-    reporters: ['dots'],
-
-    logLevel: config.LOG_INFO,
-
     autoWatch: true,
 
-    singleRun: false,
+    browsers: ['Chrome'],
 
-    customLaunchers: {
-      TRAVIS_CHROME: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
 
-    browsers: process.env.TRAVIS ? ['TRAVIS_CHROME'] : ['Chrome']
+    // reporters: ['dots'],
+    //
+    // logLevel: config.LOG_INFO,
+    //
+    // autoWatch: true,
+    //
+    // singleRun: false,
+    //
+    // customLaunchers: {
+    //   TRAVIS_CHROME: {
+    //     base: 'Chrome',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
+    //
+    // browsers: process.env.TRAVIS ? ['TRAVIS_CHROME'] : ['Chrome']
   });
 };
