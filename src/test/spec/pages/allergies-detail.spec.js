f2bcbecf5e28bfbfdba21d701f61f7fcd3614d48
Anastasiia <aholopolos@gmail.com>
2016-11-16 14:11:11 +0200

    test allergies-detail

---
+++ src/test/spec/pages/allergies-detail.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+import AllergiesDetailComponent from '../../../app/rippleui/pages/allergies/allergies-detail.component.js';
+import '../../../app/index';
+
+describe('Allergies Module', function() {
+
+  beforeEach(angular.mock.module('ripple-ui'));
+
+  let scope, ctrl, controller, template, stateParams, state, ngRedux, allergiesActions, AllergiesModal, usSpinnerService;
+
+  beforeEach(inject(($injector, $controller, _$state_, _$stateParams_, _$ngRedux_, _allergiesActions_, _AllergiesModal_, _usSpinnerService_) => {
+    controller = $controller;
+    scope = $injector.get('$rootScope').$new();
+    state = _$state_;
+    ngRedux = _$ngRedux_;
+    stateParams = _$stateParams_;
+    allergiesActions = _allergiesActions_;
+    AllergiesModal = _AllergiesModal_;
+    usSpinnerService = _usSpinnerService_;
+
+    template = AllergiesDetailComponent.template;
+    ctrl = controller(AllergiesDetailComponent.controller, {
+      $scope: scope,
+      $state: state,
+      $stateParams: stateParams,
+      $ngRedux: ngRedux,
+      allergiesActions: allergiesActions,
+      AllergiesModal: AllergiesModal,
+      usSpinnerService: usSpinnerService
+    });
+  }));
+
+  it('formDisabled', function() {
+    expect(scope.formDisabled).toBe(true);
+  });
+  it('UnlockedSources', function() {
+    expect(scope.UnlockedSources[0]).toBe('handi.ehrscape.com');
+  });
+  it('Template exist', function() {
+    expect(template).toBeDefined();
+  });
+});
