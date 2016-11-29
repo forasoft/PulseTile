14fceab07f3bd0394893e5192e2f90b54b2c5bb4
Anastasiia <aholopolos@gmail.com>
2016-11-29 10:07:51 +0200

    added test cases for HeightAndWeight module

--- src/test/spec/pages/height-and-weight/heightAndWeight-list.spec.js
+++ src/test/spec/pages/height-and-weight/heightAndWeight-list.spec.js
@@ -1,12 +1,15 @@
 'use strict';
 import HeightAndWeightListComponent from '../../../../app/rippleui/pages/height-and-weight/heightAndWeight-list.component.js';
 import '../../../../app/index';
+import * as types from '../../../../app/constants/ActionTypes';
+import heightAndWeight from '../../../../app/rippleui/pages/height-and-weight/heightAndWeight-reducer-all.js';
+import '../../../../app/index';
 
 describe('HeightAndWeight List', function() {
 
   beforeEach(angular.mock.module('ripple-ui'));
 
-  let scope, ctrl, controller, template, stateParams, state, ngRedux, heightAndWeightActions, serviceRequests, HeightAndWeightModal, usSpinnerService;
+  let scope, ctrl, controller, template, actions, fakeCall, stateParams, state, ngRedux, heightAndWeightActions, serviceRequests, HeightAndWeightModal, usSpinnerService;
 
   beforeEach(inject(($injector, $controller, _$state_, _$stateParams_, _$ngRedux_, _heightAndWeightActions_, _serviceRequests_, _HeightAndWeightModal_, _usSpinnerService_) => {
     controller = $controller;
@@ -31,8 +34,32 @@ describe('HeightAndWeight List', function() {
       HeightAndWeightModal: HeightAndWeightModal,
       usSpinnerService: usSpinnerService
     });
+    actions = $injector.get('heightAndWeightActions');
     // scope.$digest();
   }));
+  beforeEach(function() {
+    fakeCall = {
+      callHeightAndWeight: heightAndWeight
+    };
+
+    spyOn(fakeCall, 'callHeightAndWeight');
+
+    spyOn(ctrl, 'pageChangeHandler');
+    spyOn(ctrl, 'go');
+    spyOn(ctrl, 'selected');
+    spyOn(ctrl, 'search');
+    spyOn(ctrl, 'create');
+    spyOn(ctrl, 'setCurrentPageData');
+
+    fakeCall.callHeightAndWeight({}, types.HEIGHTANDWEIGHT);
+
+    ctrl.pageChangeHandler();
+    ctrl.go();
+    ctrl.selected();
+    ctrl.search();
+    ctrl.create();
+    ctrl.setCurrentPageData();
+  });
 
   it('currentPage', function() {
     expect(ctrl.currentPage).toBe(1);
@@ -42,5 +69,29 @@ describe('HeightAndWeight List', function() {
   });
   it('Controller exist', function() {
     expect(ctrl).toBeDefined();
+  });
+  it('Include heightAndWeightActions in index actions file', function() {
+    expect(actions).toBeDefined();
+  });
+  it("HeightAndWeight reducer was called", function() {
+    expect(fakeCall.callHeightAndWeight).toHaveBeenCalled();
+  });
+  it("pageChangeHandler was called", function() {
+    expect(ctrl.pageChangeHandler).toHaveBeenCalled();
+  });
+  it("route go was called", function() {
+    expect(ctrl.go).toHaveBeenCalled();
+  });
+  it("selected was called", function() {
+    expect(ctrl.selected).toHaveBeenCalled();
+  });
+  it("create was called", function() {
+    expect(ctrl.create).toHaveBeenCalled();
+  });
+  it("search was called", function() {
+    expect(ctrl.search).toHaveBeenCalled();
+  });
+  it("setCurrentPageData was called", function() {
+    expect(ctrl.setCurrentPageData).toHaveBeenCalled();
   });
 });
--- src/test/spec/pages/height-and-weight/heightAndWeight-modal.spec.js
+++ src/test/spec/pages/height-and-weight/heightAndWeight-modal.spec.js
@@ -1,12 +1,11 @@
 'use strict';
-import HeightAndWeightModal from '../../../../app/rippleui/pages/height-and-weight/heightAndWeight-modal';
 import '../../../../app/index';
 
 describe('HeightAndWeight Modal', function() {
 
   beforeEach(angular.mock.module('ripple-ui'));
 
-  let scope, controller, uibModal, heightAndWeightActions, ngRedux, stateParams;
+  let scope, controller, uibModal, heightAndWeightActions, ngRedux, stateParams, HeightAndWeightModal;
 
   beforeEach(inject(($injector, $controller, _$uibModal_, _heightAndWeightActions_, _$ngRedux_, _$stateParams_) => {
     controller = $controller;
@@ -16,8 +15,18 @@ describe('HeightAndWeight Modal', function() {
     stateParams = _$stateParams_;
     heightAndWeightActions = _heightAndWeightActions_;
   }));
+  beforeEach(function() {
+    HeightAndWeightModal = scope;
+    
+    spyOn(HeightAndWeightModal, 'openModal');
+
+    HeightAndWeightModal.openModal();
+  });
 
   it('HeightAndWeight Modal component exist', function() {
-    expect(HeightAndWeightModal).toBeDefined();
+    expect(scope).toBeDefined();
+  });
+  it('openModal was called', function() {
+    expect(HeightAndWeightModal.openModal).toHaveBeenCalled();
   });
 });
