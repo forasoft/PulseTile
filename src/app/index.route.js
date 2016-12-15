06d31c93f29650d24e1d7375c696c2997d01dda4
Alexey Honcharov <ahoncharov@sliceart.com.ua>
2016-12-15 16:27:01 +0200

    Remove last charcter from url. if it is a slash

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -16,6 +16,17 @@
 routeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 function routeConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.rule(function($injector, $location) {
+    var path = $location.path();
+    var hasTrailingSlash = path[path.length-1] === '/';
+
+    if (hasTrailingSlash) {
+      //if last charcter is a slash, return the same url without the slash  
+      var newPath = path.substr(0, path.length - 1); 
+      return newPath; 
+    } 
+  });
+  
   $urlRouterProvider.otherwise('/search');
   console.log('routing test');
   $stateProvider
