f285ca85fabf1a8b970f1931559183ba25546716
vadimivanov <Vjcrdf0508>
2017-01-10 16:24:35 +0200

    fix package.json

--- webpack.karma.contex.js
+++
@@ -1,7 +0,0 @@
-// import angular from 'angular';
-// import mocks from 'angular-mocks';
-//
-// import './src/index';
-
-let context = require.context('./src', true, /\.spec\.js/);
-context.keys().forEach(context);
